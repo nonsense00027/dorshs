b0255ab73235c423cf0c73fb18819dd7293b225e
Jaco-Miras <miras.jaco@gmail.com>
2021-04-26 20:10:26 +0800

    created login layout

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,8 +1,36 @@
-import React from "react";
-import "./Authentication.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-function Login() {
-  return <div className="login"></div>;
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function Login() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Student" />
+        <Tab label="Teacher" />
+        <Tab label="Admin" />
+      </Tabs>
+    </Paper>
+  );
 }
-
-export default Login;
