f6e127e9d83ce78dcc9ce27ffe8cd3c00ba724cf
nonsense00027 <caneteneilfred@gmail.com>
2021-05-18 13:33:54 -0700

    created student dashboard component

--- src/pages/Student/Student.js
+++ src/pages/Student/Student.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Student.css";
+import Dashboard from "./Dashboard";
 
 function Student() {
-  return <div className="student"></div>;
+  return (
+    <div className="student">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default Student;
