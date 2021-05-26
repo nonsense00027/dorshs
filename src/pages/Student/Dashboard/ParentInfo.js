0701b4a11387cc25da7fe305e26ff58f9e71d5d2
Jaco-Miras <miras.jaco@gmail.com>
2021-05-26 20:26:07 +0800

    ABC Doned

--- src/pages/Student/Dashboard/ParentInfo.js
+++ src/pages/Student/Dashboard/ParentInfo.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { FormGroup } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
 
-function ParentInfo() {
-    return (
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(2),
+      width: '35ch',
+    },
+  },
+}));
+
+export default function BasicTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
         <div>
-            <h1>Parent Info</h1>
+        <p>
+                Father's Fullname
+        </p>
         </div>
-    )
+        <div>
+        <TextField disabled id="outlined-disabled" label="Father's Last name" defaultValue="Miras" variant="outlined" />
+        </div>
+        <div>
+        <TextField disabled id="outlined-disabled" label="Father's First name" defaultValue="Conrado" variant="outlined" />
+        </div>
+        <div>
+        <TextField disabled id="outlined-disabled" label="Father's Middle name" defaultValue="Serban" variant="outlined" />
+        </div>
+        <div>
+            <p>
+            Father's Highest Education Attainment
+            </p> 
+            <FormGroup>
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Elementary graduate" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="High School graduate" /> 
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="College graduate" /> 
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Vocational" /> 
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Masters/ Doctorate degree" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Did not attend school" />      
+            <TextField disabled id="outlined-disabled" label="others" defaultValue="others" variant="outlined" /> 
+            </FormGroup>
+        <div>
+            <p>
+                Employment Status
+            </p>
+            <FormGroup>
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Full time" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Part time" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Self Employed (i.e family business)" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Unemployed due to community quarantine" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Not Working" />
+            </FormGroup>
+        </div>
+        <div>
+            <p>
+                Working from due to community quarantine
+            </p>
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Yes" />
+            <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="No" />
+        </div>
+        <div>
+            <p>
+                Contact Number
+            </p>
+            <TextField disabled id="outlined-disabled" label="Contact Number" defaultValue="+639483879921" variant="outlined" /> 
+        </div>
+        </div>
+    </form>
+  );
 }
-
-export default ParentInfo
--- src/pages/Student/Dashboard/SchoolInfo.js
+++ src/pages/Student/Dashboard/SchoolInfo.js
@@ -12,6 +12,12 @@ const useStyles = makeStyles((theme) => ({
       width: '30ch',
     },
   },
+  textField: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(2),
+      width: '30ch',
+  },
+},
 }));
 
 export default function FormPropsTextFields() {
@@ -23,32 +29,32 @@ export default function FormPropsTextFields() {
             Grade Level And School Information
         </h4>
       <div>
-        <TextField disabled id="standard-disabled" label="School Year" defaultValue="2020-2021" variant="outlined" />
+        <TextField fullWidth disabled id="standard-disabled" label="School Year" defaultValue="2020-2021" variant="outlined" />
         <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="No LRN" />
         <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="With LRN" />
         <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Returning (Balik-Aral)" />
       </div>
       <div>
-      <TextField disabled id="standard-disabled" label="Grade Level to enroll" defaultValue="Grade 12" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="Last Grade level completed" defaultValue="Grade 12" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="Last School Year completed" defaultValue="2020-2021" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="Last School Attended" defaultValue="UIC" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="School Id" defaultValue="180000002712" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="School Address" defaultValue="Father Selga St." variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="Grade Level to enroll" defaultValue="Grade 12" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="Last Grade level completed" defaultValue="Grade 12" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="Last School Year completed" defaultValue="2020-2021" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="Last School Attended" defaultValue="UIC" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="School Id" defaultValue="180000002712" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="School Address" defaultValue="Father Selga St." variant="outlined" />
       <p>School Type</p>
       <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Public" />
       <FormControlLabel disabled control={<Checkbox checked name="checkedE" />} label="Private" />
-      <TextField disabled id="standard-disabled" label="School to enroll in" defaultValue="UIC" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="School Id" defaultValue="180000002712" variant="outlined" />
-      <TextField disabled id="standard-disabled" label="School Address" defaultValue="Father Selga St." variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="School to enroll in" defaultValue="UIC" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="School Id" defaultValue="180000002712" variant="outlined" />
+      <TextField fullWidth disabled id="standard-disabled" label="School Address" defaultValue="Father Selga St." variant="outlined" />
       </div>
       <div>
           <h4>
               FOR SENIOR HIGH SCHOOL ONLY
           </h4>
-          <TextField disabled id="standard-disabled" label="Semester (1st/2nd)" defaultValue="1st" variant="outlined" />
-          <TextField disabled id="standard-disabled" label="Track" defaultValue="Technical Vocational Livelihood" variant="outlined" />
-          <TextField disabled id="standard-disabled" label="Strand (if any)" defaultValue="ICT" variant="outlined" />
+          <TextField fullWidth disabled id="standard-disabled" label="Semester (1st/2nd)" defaultValue="1st" variant="outlined" />
+          <TextField fullWidth disabled id="standard-disabled" label="Track" defaultValue="Technical Vocational Livelihood" variant="outlined" />
+          <TextField fullWidth disabled id="standard-disabled" label="Strand (if any)" defaultValue="ICT" variant="outlined" />
       </div>
     </form>
   );
