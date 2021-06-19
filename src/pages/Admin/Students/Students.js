d852dd3c13b6da79e023676d3c4f4db1eb0b301a
nonsense00027 <caneteneilfred@gmail.com>
2021-06-19 19:51:41 +0800

    created students table

--- src/pages/Admin/Students/Students.js
+++ src/pages/Admin/Students/Students.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Students.css";
+import StudentsTable from "./StudentsTable";
+import { useAdminContext } from "../../../context/AdminContext";
 
 function Students() {
-  return <div></div>;
+  const { students } = useAdminContext();
+  const getStudents = () => {
+    return students.filter((student) => student.newEnrollee === false);
+  };
+  return (
+    <div className="students">
+      <StudentsTable students={getStudents()} />
+    </div>
+  );
 }
 
 export default Students;
---
+++ src/pages/Admin/Students/StudentsTable.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  IconButton,
+  Paper,
+  Snackbar,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  withStyles,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
+import { db } from "../../../shared/configs/firebase";
+import LaunchIcon from "@material-ui/icons/Launch";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const columns = [
+  { id: "lrnNo", label: "LRN No.", minWidth: 100 },
+  { id: "lastname", label: "Lastname", minWidth: 160 },
+  { id: "firstname", label: "Firstname", minWidth: 160 },
+];
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "#223b23",
+    color: theme.palette.common.white,
+    fontSize: "15px",
+    fontWeight: "600",
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(even)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  root: {
+    width: "90%",
+  },
+  container: {
+    maxHeight: 1000,
+  },
+});
+
+function StudentsTable({ students }) {
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <div className="examTable">
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table" size="small">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <StyledTableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </StyledTableCell>
+                ))}
+                <StyledTableCell key="actions" style={{ minWidth: 50 }}>
+                  Actions
+                </StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {students
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <StyledTableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.id}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                      <TableCell key={row.id} align="left">
+                        <IconButton>
+                          <LaunchIcon
+                            onClick={() =>
+                              window.open(
+                                // `${window.location.href}/${row.id}`,
+                                `/student/${row.lrnNo}`,
+                                "_blank"
+                              )
+                            }
+                          />
+                        </IconButton>
+                        {/* <IconButton onClick={() => handleDeleteDialogOpen(row)}>
+                          <DeleteIcon />
+                        </IconButton> */}
+                      </TableCell>
+                    </StyledTableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 15, 20]}
+          component="div"
+          count={students.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+}
+
+export default StudentsTable;
