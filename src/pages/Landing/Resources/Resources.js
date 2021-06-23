2e172641ba879e2b9c0697c6d34cffde496465f8
nonsense00027 <caneteneilfred@gmail.com>
2021-06-23 09:25:18 +0800

    adjusted layout in the resources page

--- src/pages/Landing/Resources/Resources.js
+++ src/pages/Landing/Resources/Resources.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Details from './Details';
+import React from "react";
+import Details from "./Details";
+import Navbar from "../Navbar";
 
 function Resources() {
-    return (
-        <div className='resources'>
-            <Details />
-        </div>
-    )
+  return (
+    <div className="resources">
+      <Navbar />
+      <Details />
+    </div>
+  );
 }
 
-export default Resources 
+export default Resources;
