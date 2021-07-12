8a7401bfc457d77589c5d6f3277c704593e8c676
nonsense00027 <caneteneilfred@gmail.com>
2021-07-12 13:01:40 +0800

    added all teachers assets

--- src/shared/teachersData.js
+++ src/shared/teachersData.js
@@ -1,6 +1,32 @@
 import briones from "../img/teachers/briones.jpg";
 import samson from "../img/teachers/samson.jpg";
 import dumaran from "../img/teachers/dumaran.jpg";
+import torres from "../img/teachers/torres.jpg";
+import no from "../img/teachers/no.jpg";
+import montebon from "../img/teachers/montebon.jpg";
+import laguidong from "../img/teachers/laguidong.jpg";
+import adamos from "../img/teachers/adamos.jpg";
+import entrino from "../img/teachers/entrino.jpg";
+import andress from "../img/teachers/andress.jpg";
+import deborah from "../img/teachers/deborah.jpg";
+import mejos from "../img/teachers/mejos.jpg";
+import morales from "../img/teachers/morales.jpg";
+import luna from "../img/teachers/luna.jpg";
+import mamac from "../img/teachers/mamac.jpg";
+import siason from "../img/teachers/siason.jpg";
+import cuevas from "../img/teachers/cuevas.jpg";
+import flores from "../img/teachers/flores.jpg";
+import pareja from "../img/teachers/pareja.jpg";
+import dizon from "../img/teachers/dizon.jpg";
+import nonol from "../img/teachers/nonol.jpg";
+import sibay from "../img/teachers/sibay.jpg";
+import parameo from "../img/teachers/parameo.jpg";
+import lopez from "../img/teachers/lopez.jpg";
+import sagpang from "../img/teachers/sagpang.jpg";
+import burgos from "../img/teachers/burgos.jpg";
+import quibo from "../img/teachers/quibo.jpg";
+import lumpay from "../img/teachers/lumpay.jpg";
+import bariera from "../img/teachers/bariera.jpg";
 
 export default [
   {
@@ -20,5 +46,209 @@ export default [
     position: "Senior High School Focal Person",
     category: "Master Teacher II, Chemistry, Biology",
     image: dumaran,
+  },
+  {
+    name: "Analisa Mocoy",
+    position: "Teacher",
+    category: "Secondary School Teacher III, Math 7, ESP 8",
+    image: no,
+  },
+  {
+    name: "Lorie Mae Babiera",
+    position: "Teacher",
+    category: "Secondary School Teacher I, Science 7, Mapeh 7",
+    image: bariera,
+  },
+  {
+    name: "Roxanne Lee Lumpay",
+    position: "Teacher",
+    category: "Secondary School Teacher I, Science 7, Araling Panlipunang 7",
+    image: lumpay,
+  },
+  {
+    name: "Johannes Latras",
+    position: "Teacher",
+    category:
+      "Secondary School Teacher I ,Araling Panlipunan 10, Research 8 and 7",
+    image: no,
+  },
+  {
+    name: "Elvira Q. Quibo",
+    position: "Teacher",
+    category: "Secondary School Teacher I, English 7, Filipino 7",
+    image: quibo,
+  },
+  {
+    name: "Lotus Burgos",
+    position: "Teacher",
+    category:
+      "Secondary School Teacher III, Science 7, Mapeh 8, Araling Panlipunan9",
+    image: burgos,
+  },
+  {
+    name: "Marife Sagpang",
+    position: "Teacher",
+    category: "Secondary School Teacher II, English 8 & 10, ESP 8",
+    image: sagpang,
+  },
+  {
+    name: "John Mark Lopez",
+    position: "Teacher",
+    category: "Secondary School Teacher II, Science 8, Research 8, ESP 9",
+    image: lopez,
+  },
+  {
+    name: "Honey Mae Paramio",
+    position: "Teacher",
+    category: "Principall II, econdary School Teacher I, English 8 & 10, ESP 8",
+    image: parameo,
+  },
+  {
+    name: "Stephanie Sibay	",
+    position: "Teacher",
+    category: "English 8 & 10",
+    image: sibay,
+  },
+  {
+    name: "Emily Nonol",
+    position: "Teacher",
+    category: "Secondary School Teacher III, English 9, Filipino 8, ESP 9",
+    image: nonol,
+  },
+  {
+    name: "Princess Farridah Duaran",
+    position: "Teacher",
+    category:
+      "Secondary School Teacher I, English 9, Mapeh 10, Araling Panlipunan 9",
+    image: no,
+  },
+  {
+    name: "Reward Dizon",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Research 9, Science 9",
+    image: dizon,
+  },
+  {
+    name: "Michelle Pareja",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Mapeh 9, Science 8",
+    image: pareja,
+  },
+  {
+    name: "Melbert Flores",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Math 9, ESP 9",
+    image: flores,
+  },
+  {
+    name: "Aida Cuevas",
+    position: "Teacher",
+    category: "Secondary School Teacher III",
+    subjects: "Science 9 & 10 , ESP 10",
+    image: cuevas,
+  },
+  {
+    name: "Haidee Siason",
+    position: "Teacher",
+    category: "Secondary School Teacher II",
+    subjects: "Science 10, ESP 10",
+    image: siason,
+  },
+  {
+    name: "Romeo Mamac",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Filipino 9 & 10",
+    image: mamac,
+  },
+  {
+    name: "Gene Pearl Luna",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Research 10, ESP 7",
+    image: luna,
+  },
+  {
+    name: "Louie Niez",
+    position: "Teacher",
+    category: "Secondary School Teacher I",
+    subjects: "Math 10, ESP 10",
+    image: no,
+  },
+  {
+    name: "Leah Dela Rosa",
+    position: "Teacher",
+    category: "Principall II, Master Teacher II",
+    subjects: "PE I",
+    image: no,
+  },
+  {
+    name: "Sharon Morales",
+    position: "Teacher",
+    category: "Secondary School Teacher III",
+    subjects: "Filipino subjects",
+    image: morales,
+  },
+  {
+    name: "Ludy Jane Mejos",
+    position: "Teacher",
+    category: "Secondary School Teacher III",
+    subjects: "DRRM, PE, 21ST 	Century",
+    image: mejos,
+  },
+  {
+    name: "Deborrah Ann Milan",
+    position: "Teacher",
+    category: "Secondary School Teacher III",
+    subjects: "Per Dev, Reading & Writing",
+    image: deborah,
+  },
+  {
+    name: "Lloyd Andres",
+    position: "Teacher",
+    category: "Master Teacher I",
+    subjects: "Pre calculus, Basic Calculus, Probability and Statistics",
+    image: andress,
+  },
+  {
+    name: "Kristal Entrino",
+    position: "Teacher",
+    category: "Master Teacher I",
+    subjects: "Practical Research 2, Reading & Writing",
+    image: entrino,
+  },
+  {
+    name: "Wilmar Adamos",
+    position: "Teacher",
+    category: "Secondary School Teacher III",
+    subjects: "Physics & Entrepreneurship",
+    image: adamos,
+  },
+  {
+    name: "Jade Mariz Laguidong",
+    position: "Administrator",
+    category: "Administrative Assistant III",
+    image: laguidong,
+  },
+  {
+    name: "Daryl Montebon",
+    position: "Administrator",
+    category: "Adminitrative Assistant II",
+    image: montebon,
+  },
+  {
+    name: "Realyn Relatado",
+    position: "Administrator",
+    category: "Adminitrative Assistant II",
+    image: no,
+  },
+  {
+    name: "Nikka Gwen Torres",
+    position: "Administrator",
+    category: "Adminitrative Assistant II",
+    image: torres,
   },
 ];
