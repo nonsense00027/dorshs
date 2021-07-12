7d69bc3032c8de96f0f7bff5e9c297a7be83aa29
Jaco-Miras <miras.jaco@gmail.com>
2021-07-12 09:27:23 +0800

    Added Features

--- src/shared/teachersData.js
+++ src/shared/teachersData.js
@@ -1,6 +1,16 @@
 import briones from "../img/teachers/briones.jpg";
 import samson from "../img/teachers/samson.jpg";
 import dumaran from "../img/teachers/dumaran.jpg";
+import nopic from '../img/teachers/nopic.jpg';
+import barbiera from '../img/teachers/barbiera.jpg';
+import lumpay from '../img/teachers/lumpay.jpg'
+import quibo from '../img/teachers/quibo.jpg';
+import burgos from '../img/teachers/burgos.jpg';
+import sagpang from '../img/teachers/sagpang.jpg';
+import lopez from '../img/teachers/lopez.jpg';
+import parameo from '../img/teachers/parameo.jpg';
+import sibay from '../img/teachers/sibay.jpg';
+import nonol from '../img/teachers/nonol.jpg';
 
 export default [
   {
@@ -21,4 +31,95 @@ export default [
     category: "Master Teacher II, Chemistry, Biology",
     image: dumaran,
   },
+
+  {
+    name: "Analisa Mocoy",
+    position: "Teacher",
+    category: "Secondary School Teacher III, Math 7, ESP 8",
+    image: nopic,
+  },
+
+  {
+    name: "Lorie Mae Barbiera",
+    position: "Teacher",
+    category: "Secondary School Teacher I, Science 7, Mapeh 7",
+    image: barbiera,
+  },
+
+  {
+    name: "Roxanne Lee Lumpay",
+    position: "Teacher",
+    category: "Secondary School Teacher I, Science 7, Araling Panlipunan 7",
+    image: lumpay,
+  },
+
+  {
+    name: "Johannes Latras",
+    position: "Teacher",
+    category: "Secondary School Teacher I, Araling Panlipunan 10, Research 7 and 8",
+    image: nopic,
+  },
+
+  {
+    name: "Elvira Q. Quibo",
+    position: "Teacher",
+    category: "Secondary School Teacher I, English 7, Filipino 7",
+    image: quibo,
+  },
+
+  {
+    name: "Lotus Burgos",
+    position: "Teacher",
+    category: "Secondary School Teacher III, Science 7, Mapeh 8, Araling Panlipunan 9",
+    image: burgos,
+  },
+
+  {
+    name: "Marife Sagpang",
+    position: "Teacher",
+    category: "Secondary School Teacher II, English 8 & 10, ESP 8",
+    image: sagpang,
+  },
+
+  {
+    name: "John Mark Lopez",
+    position: "Teacher",
+    category: "Secondary School Teacher II, Science 8, Research 8, ESP 9",
+    image: lopez,
+  },
+
+  {
+    name: "Honey Mae Paramio",
+    position: "Teacher",
+    category: "Principal II, Secondary School Teacher I, English 8 & 10, ESP 8",
+    image: parameo,
+  },
+
+  {
+    name: "Stephanie Sibay",
+    position: "Teacher",
+    category: "English 8 & 10",
+    image: sibay,
+  },
+
+  {
+    name: "Emily Nonol",
+    position: "Teacher",
+    category: "Secondary School Teacher III, English 9, Filipino 8, ESP 9",
+    image: nonol,
+  },
+
+  {
+    name: "Princess Farridah Duaran",
+    position: "Teacher",
+    category: "Secondary School Teacher I, English 9, Mapeh 10, Araling Panlipunan 9",
+    image: nopic,
+  },
+
+  // {
+  //   name: "",
+  //   position: "",
+  //   category: "",
+  //   image: ,
+  // },
 ];
